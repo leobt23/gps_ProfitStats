d0706f5f621aa0e18bd143be99fdc7cb6234b585
filipealmeida20 <a21280720@isec.pt>
2020-12-14 09:44:47 +0000

    Commit test

---
+++ src/UserTest.java
@@ -0,0 +1,53 @@
+import org.junit.Test;
+import org.testng.Assert;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    public void getName() {
+        User user = new User("Filipe", "a123@isec.pt", "Male", 20, 0);
+        Assert.assertEquals("Filipe", user.getName());
+    }
+
+    @org.testng.annotations.Test
+    void setName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getGender() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setGender() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTotalBets() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setTotalBets() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
