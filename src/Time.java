1e84b9ac9b66c75da65512a73e18938e1622c04d
Sousa2906-code <73475521+Sousa2906-code@users.noreply.github.com>
2020-12-14 09:21:37 +0000

    Add files via upload

---
+++ src/Time.java
@@ -0,0 +1,91 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Time {
+    int year, month, week, day, hour, minute, seconds;
+
+    public Time(int year, int month, int week, int day, int hour, int minute, int seconds) {
+        this.year = year;
+        this.month = month;
+        this.week = week;
+        this.day = day;
+        this.hour = hour;
+        this.minute = minute;
+        this.seconds = seconds;
+    }
+
+    public String getCurrentDate(){
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        return formatter.format(date);
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getWeek() {
+        return week;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setWeek(int week) {
+        this.week = week;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    @Override
+    public String toString() {
+        return "TIme{" +
+                "year=" + year +
+                ", month=" + month +
+                ", week=" + week +
+                ", day=" + day +
+                ", hour=" + hour +
+                ", minute=" + minute +
+                ", seconds=" + seconds +
+                '}';
+    }
+}
