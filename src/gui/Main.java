efe39b3ab8ce0ae6c3ed93c07fd309549cd0836e
Filipe Almeida <faracsb00@gmail.com>
2020-12-20 11:44:44 +0000

    Minor changes

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -8,13 +8,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         Root root = new Root();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
--- src/gui/Root.java
+++ src/gui/Root.java
@@ -5,8 +5,7 @@ import javafx.scene.layout.BorderPane;
 
 public class Root extends BorderPane {
 
-    public Root()
-    {
+    public Root() {
         setPrefSize(1000,600);
         LeftPanel leftPanel = new LeftPanel();
         setLeft(leftPanel);
