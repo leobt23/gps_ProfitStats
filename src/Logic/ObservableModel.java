9425a02f455d8c87b091dff3db6ed26345ca1482
Filipe Almeida <faracsb00@gmail.com>
2020-12-20 13:20:53 +0000

    ObservableModel
    Bets history functionality implemented

--- src/Logic/ObservableModel.java
+++ src/Logic/ObservableModel.java
@@ -15,4 +15,29 @@ public class ObservableModel {
     public String getBetId(int idx) {
         return data.getBetName(idx);
     }
+
+    public String getBetRegistryDate(int idx) {
+        return data.getBetRegistryDate(idx);
+    }
+
+    public String getBetNumberOfGames(int idx) {
+        return data.getBetNumberOfGames(idx);
+    }
+
+    public String getBetNumberOfBets(int idx) {
+        return data.getBetNumberOfBets(idx);
+    }
+
+    public String getBetValueBetted(int idx) {
+        return data.getBetValueBetted(idx);
+    }
+
+    public String getBetPossibleWinnings(int idx) {
+        return data.getBetPossibleWinnings(idx);
+    }
+
+    public String getBetStatus(int idx) {
+        return data.getBetStatus(idx);
+    }
+
 }
