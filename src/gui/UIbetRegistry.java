8c1b8827b57d921d2fbfcbcd19514bfde517b39c
MrSano <samjorge99@gmail.com>
2020-12-23 00:25:39 +0000

    UIBetRegistry missing wrong input only(i guess) 3 hours? maybe

--- src/gui/UIbetRegistry.java
+++ src/gui/UIbetRegistry.java
@@ -10,7 +10,6 @@ import javafx.scene.input.MouseButton;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import logic.ObservableModel;
@@ -20,7 +19,6 @@ import logic.states.EnumStates;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.time.LocalDate;
-import java.util.ArrayList;
 
 public class UIbetRegistry extends BorderPane {
     private ObservableModel obsModel;
