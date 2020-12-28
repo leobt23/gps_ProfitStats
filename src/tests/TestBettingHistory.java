51e016bc43213218f5a6a3a26113abac60b7e5f7
Leonardo Amado Brito <leo_brito32@hotmail.com>
2020-12-28 15:49:50 +0000

    Test BettingHistory class

--- src/tests/TestBettingHistory.java
+++ src/tests/TestBettingHistory.java
@@ -48,10 +48,10 @@ public class TestBettingHistory {
 
     @Test
     void findBet() {
-        Time time = new Time(2021,1, 1, 1, 1,1,1);
-        Time time1 = new Time(2022,2, 2, 2, 2,2,2);
-        Time time2 = new Time(2023,3, 3, 3, 3,3,3);
-        Time time3 = new Time(2024,4, 4, 4, 4,4,4);
+        Time time = new Time(2021,1, 1, 1);
+        Time time1 = new Time(2022,2, 2, 2);
+        Time time2 = new Time(2023,3, 3, 3);
+        Time time3 = new Time(2024,4, 4, 4);
         Bet bet = new Bet(2, 2, time, time1, 2, 4, "bet1", EnumBetStatus.WON);
         Bet bet2 = new Bet(2, 2, time, time1, 2, 4, "bet2", EnumBetStatus.WON);
         BettingHistory a = new BettingHistory() ;
@@ -64,10 +64,10 @@ public class TestBettingHistory {
 
     @Test
     void removeBet() {
-        Time time = new Time(2021,1, 1, 1, 1,1,1);
-        Time time1 = new Time(2022,2, 2, 2, 2,2,2);
-        Time time2 = new Time(2023,3, 3, 3, 3,3,3);
-        Time time3 = new Time(2024,4, 4, 4, 4,4,4);
+        Time time = new Time(2021,1, 1, 1);
+        Time time1 = new Time(2022,2, 2, 2);
+        Time time2 = new Time(2023,3, 3, 3);
+        Time time3 = new Time(2024,4, 4, 4);
         Bet bet = new Bet(2, 2, time, time1, 2, 4, "bet1", EnumBetStatus.WON);
         Bet bet2 = new Bet(2, 2, time, time1, 2, 4, "bet2", EnumBetStatus.WON);
         BettingHistory a = new BettingHistory() ;
